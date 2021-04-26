7430ade11cbe4a3ecdfa39be4c9fb687242dfb80
Jacobo <jacoboados16@gmail.com>
2021-04-26 19:46:24 +0200

    Añadido endpoint Countries

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class HelloController {
     
 	private final List<City> cities = new ArrayList<City>();
+	private final List<Country> countries = new ArrayList<Country>();
 
 	public HelloController(){
 		City A = new City(1, "Coruna", "A Coruna");
@@ -28,6 +29,28 @@ public class HelloController {
 		this.cities.add(E);
 		this.cities.add(F);
 		this.cities.add(G);
+
+		Country a = new Country(1, "Espana", "Madrid");
+		Country b = new Country(2, "Francia", "Paris");
+		Country c = new Country(3, "Italia", "Roma");
+		Country d = new Country(4, "Grecia", "Atenas");
+		Country e = new Country(5, "Croacia", "Zagreb");
+		Country f = new Country(6, "Malta", "La Valeta");
+		Country g = new Country(7, "Albania", "Tirana");
+		Country h = new Country(8, "Chipre", "Nicosia");
+		Country i = new Country(9, "Eslovenia", "Ljubliana");
+		Country j = new Country(10, "Montenegro", "Podgorica");
+
+		this.countries.add(a);
+		this.countries.add(b);
+		this.countries.add(c);
+		this.countries.add(d);
+		this.countries.add(e);
+		this.countries.add(f);
+		this.countries.add(g);
+		this.countries.add(h);
+		this.countries.add(i);
+		this.countries.add(j);
 	}
 
 	@GetMapping("/cities")
@@ -39,6 +62,11 @@ public class HelloController {
 	@GetMapping("/cities/{id}")
 	public City City(@PathVariable int id) {
 		return cities.get(id-1);
+	}
+
+	@GetMapping("/countries")
+	public List<Country> List_countries() {
+		return this.countries;
 	}
 
 }
