dc7fbe12bef97fbb63d4e0e8d49b448aee14c488
Jacobo <jacoboados16@gmail.com>
2021-04-26 18:44:55 +0200

    Cambio run.sh

--- test/run.sh
+++ test/run.sh
@@ -3,30 +3,7 @@ cd $(dirname $0)
 
 cd ../complete
 
-mvn clean package
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
-
-./gradlew build
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
-
-cd ../initial
-
-mvn clean compile
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
-
-./gradlew compileJava
+./gradlew test --rerun-tasks --stacktrace
 ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
